19c2d3ae24b11cf1fe10a49e1306172ace165dcd
Daria_H <dholubenna@gmail.com>
2025-05-12 01:58:11 +0300

    fix js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,106 +1,103 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const datetimePicker = document.querySelector("#datetime-picker");
-const btnStart = document.querySelector("button[data-start]");
-const daysValue = document.querySelector("span[data-days]");
-const hoursValue = document.querySelector("span[data-hours]");
-const minutesValue = document.querySelector("span[data-minutes]");
-const secondsValue = document.querySelector("span[data-seconds]");
-const span = document.querySelector("span");
+const datetimePicker = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('button[data-start]');
+const daysValue = document.querySelector('span[data-days]');
+const hoursValue = document.querySelector('span[data-hours]');
+const minutesValue = document.querySelector('span[data-minutes]');
+const secondsValue = document.querySelector('span[data-seconds]');
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    btnStart.disabled = true;
-})
+document.addEventListener('DOMContentLoaded', () => {
+  validate();
+});
 let userSelectedDate = null;
+
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-      if(selectedDates[0] >= new Date()){
-        userSelectedDate = selectedDates[0];
-        btnStart.disabled = false;
-      }    else {
-        btnStart.disabled = true;
-        iziToast.error({
-            title: 'ERROR',
-            titleColor: '#fff',
-            messageColor: '#fff',
-            backgroundColor: '#ef4040',
-            message: 'Please choose a date in the future',
-            position: 'topRight',
-            color:'#fff'
-        });
-        userSelectedDate = null;
-      }
-    },
-  };
-  
-  flatpickr(datetimePicker, options);
-
- btnStart.addEventListener('click', (ev) =>{
-    if(userSelectedDate !== null){
-        btnStart.disabled = true;
-        datetimePicker.disabled = true;
-        const dateNow = new Date().getTime();
-        const currentTime = userSelectedDate - dateNow;
-        startCountdown(currentTime);
-    } else{
-        iziToast.warning({
-            title: 'Warning',
-      message: 'Please choose a valid date in the future',
-      position: 'topRight',
-      titleColor: '#fff',
-      messageColor: '#fff',
-      backgroundColor: '#ef4040'
-    })
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    validate();
+    if (userSelectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a valid date in the future',
+        position: 'topRight',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        backgroundColor: '#ef4040',
+      });
     }
- })
- let timerInterval = null;
+  },
+};
 
- function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-      const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
+flatpickr(datetimePicker, options);
+
+function validate() {
+  if (userSelectedDate === null || userSelectedDate <= new Date()) {
+    btnStart.disabled = true;
+    return !btnStart.disabled;
+  } else {
+    btnStart.disabled = false;
+    return !btnStart.disabled;
   }
-  
-  function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-  };
-  function startCountdown(duration) {
-    timerInterval = setInterval(() => { 
-      const { days, hours, minutes, seconds } = convertMs(duration);   
-      daysValue.textContent = days;
-      hoursValue.textContent = addLeadingZero(hours);
-      minutesValue.textContent = addLeadingZero(minutes);
-      secondsValue.textContent = addLeadingZero(seconds);
-  
-      duration -= 1000;   
-      if (duration < 0) {
-        clearInterval(timerInterval);
-        timerInterval = null;
-        daysValue.textContent = '0'; // Оновлюємо дні на нуль
-        hoursValue.textContent = '00';
-        minutesValue.textContent = '00';
-        secondsValue.textContent = '00';
-        datetimePicker.disabled = false;
-        iziToast.info({
-          title: 'Info',
-          message: 'Час вийшов!'
-        });
-      }
-    }, 1000);
+}
+
+btnStart.addEventListener('click', ev => {
+  if (validate()) {
+    datetimePicker.disabled = true;
+    const dateNow = new Date().getTime();
+    const currentTime = userSelectedDate - dateNow;
+    startCountdown(currentTime);
   }
+});
+let timerInterval = null;
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+function startCountdown(duration) {
+  timerInterval = setInterval(() => {
+    const { days, hours, minutes, seconds } = convertMs(duration);
+    daysValue.textContent = addLeadingZero(days);
+    hoursValue.textContent = addLeadingZero(hours);
+    minutesValue.textContent = addLeadingZero(minutes);
+    secondsValue.textContent = addLeadingZero(seconds);
+
+    duration -= 1000;
+    if (duration < 0) {
+      clearInterval(timerInterval);
+      timerInterval = null;
+      daysValue.textContent = '00';
+      hoursValue.textContent = '00';
+      minutesValue.textContent = '00';
+      secondsValue.textContent = '00';
+      datetimePicker.disabled = false;
+      iziToast.info({
+        title: 'Info',
+        message: 'Час вийшов!',
+      });
+    } else {
+      btnStart.disabled = true;
+    }
+  }, 1000);
+}
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,87 +3,62 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 
 const formData = document.querySelector('.form');
-const inputDelay = document.querySelector('input[name="delay"]');
+const delay = document.querySelector('input[name="delay"]');
 
-const valid = () => {
-  if (isNaN(inputDelay.value)) {
-    console.log('Введіть число');
+
+function valid() {
+  const delayValue = Number(delay.value);
+  if (isNaN(delayValue) || (delayValue <= 0)) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Введіть ціле число',
+      position: 'topRight',
+      messageColor: 'black',
+      color: 'lightblue',
+      titleColor: 'black'
+    })
     return false;
   } else {
-    console.log('Введено число:', Number(inputDelay.value));
+    console.log('Введено число:', delayValue);
     return true;
   }
 };
 
+
 formData.addEventListener('submit', ev => {
   ev.preventDefault();
-
   const inputStates = document.querySelector('input[name=state]:checked');
 
-  if (inputStates.value === 'fulfilled') {
+  setTimeout(() => {
     if (!valid()) return;
-    setTimeout(() => {
-      new Promise((resolve, reject) => {
-        resolve(console.log(`create with ${inputDelay.value}`));
-     
+    const delayValue = Number(delay.value);
+    const promise = new Promise((resolve, reject) => {
+      if (inputStates.value === 'fulfilled') {
+        resolve(delayValue);
+        console.log(`create with ${delayValue}`);
+      } else if(inputStates.value === 'rejected'){
+        reject(delayValue);
+        console.log(`NOT create ${delayValue}`);
+      }
     })
+      promise
       .then(res =>
         iziToast.success({
           title: 'OK',
-          message: `✅ Fulfilled promise in ${inputDelay.value}ms`,
+          message: `✅ Fulfilled promise in ${delayValue}ms`,
           position: 'topRight',
           messageColor: '#fff',
           color: '#59A10D',
           titleColor: '#fff'
         })
-      )
-      .catch(error =>
-        iziToast.show({
+      ).catch(error =>
+        iziToast.error({
           title: 'ERROR',
-          message: `❌ Rejected promise in ${inputDelay.value}ms` ,
+          message: `❌ Rejected promise in ${delayValue}ms`,
           position: 'topRight',
           messageColor: '#fff',
-           titleColor: '#fff'
+          titleColor: '#fff'
         })
       );
-       doOne();
-       }, Number(inputDelay.value));
-  } else if (inputStates.value === 'rejected') {
-    if (!valid()) return;
-    setTimeout(() => {
-      new Promise((resolve, reject) => {
-        reject(console.log(`NOT create ${inputDelay.value}`));
-      })
-        .then(res =>
-          iziToast.success({
-            title: 'OK',
-            message: `✅ Fulfilled promise in ${inputDelay.value}ms`,
-            position: 'topRight',
-            messageColor: '#fff',
-            color: '#59A10D',
-             titleColor: '#fff'
-          })
-        )
-        .catch(error =>
-          iziToast.show({
-            title: 'ERROR',
-            message: `❌ Rejected promise in ${inputDelay.value}ms`,
-            position: 'topRight',
-            color: '#FAFAFB',
-            backgroundColor: '#EF4040',
-            messageColor: '#fff',
-             titleColor: '#fff'
-          })
-        );
-      doTwo();
-    }, Number(inputDelay.value));
-  } else {
-    console.log('you must choise');
-  }
+  },Number(delay.value))
 });
-function doOne() {
-  console.log('do 1');
-}
-function doTwo() {
-  console.log('do 2');
-}
